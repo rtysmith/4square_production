d4ea51a8cb4695d4ff2a97bc5f14b96f6d8ce4d3
rtysmith <tyler@loftopia.com>
2026-08-22 15:52:19 -0400

    Software v19.11

--- firmware/foursquare/src/fw_version.h
+++ firmware/foursquare/src/fw_version.h
@@ -1,6 +1,6 @@
 #pragma once
 // Written by the 4square Control Center every time it commits a build.
 // Do not edit by hand: the next build overwrites it.
-#define FOURSQUARE_FW_BUILD   10
-#define FOURSQUARE_FW_VERSION "v19.10"
+#define FOURSQUARE_FW_BUILD   11
+#define FOURSQUARE_FW_VERSION "v19.11"
 #define FOURSQUARE_BUILT_BY   "SWYR.com"
