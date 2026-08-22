86e34c593ea3004090604020cecd828e06c8c34a
rtysmith <tyler@loftopia.com>
2026-08-22 12:21:07 -0400

    Software v16.5

--- firmware/foursquare/src/fw_version.h
+++ firmware/foursquare/src/fw_version.h
@@ -1,6 +1,6 @@
 #pragma once
 // Written by the 4square Control Center every time it commits a build.
 // Do not edit by hand: the next build overwrites it.
-#define FOURSQUARE_FW_BUILD   4
-#define FOURSQUARE_FW_VERSION "v15.4"
+#define FOURSQUARE_FW_BUILD   5
+#define FOURSQUARE_FW_VERSION "v16.5"
 #define FOURSQUARE_BUILT_BY   "SWYR.com"
