a0bc6c3aa06cc62a290a206f062f606a2fb0ce10
rtysmith <tyler@loftopia.com>
2026-08-22 17:06:12 -0400

    Software v23.13

--- firmware/foursquare/src/fw_version.h
+++ firmware/foursquare/src/fw_version.h
@@ -1,6 +1,6 @@
 #pragma once
 // Written by the 4square Control Center every time it commits a build.
 // Do not edit by hand: the next build overwrites it.
-#define FOURSQUARE_FW_BUILD   12
-#define FOURSQUARE_FW_VERSION "v19.12"
+#define FOURSQUARE_FW_BUILD   13
+#define FOURSQUARE_FW_VERSION "v23.13"
 #define FOURSQUARE_BUILT_BY   "SWYR.com"
