c29d598624cf05ef844790e829be2d5e781bea4b
rtysmith <tyler@loftopia.com>
2026-09-16 13:54:47 -0400

    Add HTTP layout/update endpoints

--- firmware/foursquare/src/net/webcfg.cpp
+++ firmware/foursquare/src/net/webcfg.cpp
@@ -47,7 +47,7 @@
 #define FOURSQUARE_BUILD_ID "unknown"
 #endif
 
-#define WEBCFG_API 28  // 28 = LinkedIn panel adds indoor humidity history
+#define WEBCFG_API 29  // 29 = LinkedIn humidity footer always drawn
 
 static WebServer  server(80);
 static bool       started  = false;
